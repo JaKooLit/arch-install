e021e7929ce5e24e6538407759fb6a1096323bc9
damdam-kr <85185940+damdam-kr@users.noreply.github.com>
2021-06-16 00:14:29 +0900

    Create budgie.sh

---
+++ budgie.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+sudo timedatectl set-ntp true
+sudo hwclock --systohc
+
+sudo reflector -c KR,HK,TW,SG,JP -a 6 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+sudo pacman -Syy
+
+sudo firewall-cmd --add-port=1025-65535/tcp --permanent
+sudo firewall-cmd --add-port=1025-65535/udp --permanent
+sudo firewall-cmd --reload
+
+
+#paru -S --noconfirm auto-cpufreq
+#sudo systemctl enable --now auto-cpufreq
+#systemctl start auto-cpufreq
+
+sudo pacman -S xorg lightdm lightdm-greeter budgie-desktop gnome-screensaver nautilus xdg-user-dirs-gtk alacritty gnome-control-center noto-fonts ttf-hack adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk firefox vlc powerline-fonts 
+
+sudo systemctl enable lightdm
+
+/bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
+#sleep 5
+reboot
