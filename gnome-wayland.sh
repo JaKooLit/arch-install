2f8b52e5badb9a7e08fe7f553f1e2888e2a04860
damdam-kr <85185940+damdam-kr@users.noreply.github.com>
2021-07-09 00:39:44 +0900

    Create gnome-wayland.sh

---
+++ gnome-wayland.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+sudo timedatectl set-ntp true
+sudo hwclock --systohc
+
+sudo reflector -c KR,HK,TW,SG,JP -a 6 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+
+sudo firewall-cmd --add-port=1025-65535/tcp --permanent
+sudo firewall-cmd --add-port=1025-65535/udp --permanent
+sudo firewall-cmd --reload
+
+
+#paru -S --noconfirm auto-cpufreq
+#sudo systemctl enable --now auto-cpufreq
+#systemctl start auto-cpufreq
+
+
+sudo pacman -S --noconfirm gdm gnome gnome-extra firefox gnome-tweaks simplescreenrecorder arc-gtk-theme arc-icon-theme obs-studio vlc dina-font tamsyn-font bdf-unifont ttf-bitstream-vera ttf-croscore ttf-dejavu ttf-droid gnu-free-fonts ttf-ibm-plex ttf-liberation ttf-linux-libertine noto-fonts ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family ttf-anonymous-pro ttf-cascadia-code ttf-fantasque-sans-mono ttf-fira-mono ttf-hack ttf-fira-code ttf-inconsolata ttf-jetbrains-mono ttf-monofur adobe-source-code-pro-fonts cantarell-fonts inter-font ttf-opensans gentium-plus-font ttf-junicode adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk noto-fonts-emoji
+
+sudo pacman -S --noconfirm xdg-desktop-portal-gtk
+
+# sudo pacman -S --noconfirm xorg
+
+sudo systemctl enable gdm
+/bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
+#sleep 5
+#sudo reboot
