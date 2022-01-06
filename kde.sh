14462132dfc6df6df2a2e76345d4cb1cfdbca1e6
Ja.KooLit <85185940+JaKooLit@users.noreply.github.com>
2022-01-06 18:57:35 +0900

    Update kde.sh

--- kde.sh
+++ kde.sh
@@ -10,15 +10,6 @@ sudo firewall-cmd --add-port=1025-65535/tcp --permanent
 sudo firewall-cmd --add-port=1025-65535/udp --permanent
 sudo firewall-cmd --reload
 
-#cant perform makepkg for root
-#git clone https://aur.archlinux.org/paru-bin.git
-#cd paru-bin
-#makepkg -si --noconfirm
-
-#paru -S --noconfirm auto-cpufreq
-#sudo systemctl enable --now auto-cpufreq
-#systemctl start auto-cpufreq
-
 #edit the packages as required
 sudo pacman -S xorg sddm plasma-desktop konsole packagekit-qt5 noto-fonts ttf-hack adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk powerdevil plasma-nm bluedevil firefox spectacle gwenview vlc kate dolphin powerline-fonts discover sddm-kcm print-manager khotkeys kscreen kgamma5 kinfocenter ksystemlog filelight
 
