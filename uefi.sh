062037c265cc2d97d4e8f926d743e046dc81d1ed
Ja.KooLit <85185940+JaKooLit@users.noreply.github.com>
2022-01-17 01:07:09 +0900

    Update uefi.sh

--- uefi.sh
+++ uefi.sh
@@ -1,13 +1,14 @@
+#!/bin/bash
 
 ln -sf /usr/share/zoneinfo/Asia/Seoul /etc/localtime
 hwclock --systohc
 sed -i '177s/.//' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
-echo "Arch" >> /etc/hostname
+echo "Asus-Arch" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
-echo "127.0.1.1 Arch.localdomain Arch" >> /etc/hosts
+echo "127.0.1.1 Asus-Arch.localdomain Asus-Arch" >> /etc/hosts
 echo root:password | chpasswd
 
 pacman -S grub base-devel efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools  avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils alsa-utils easyeffects pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call  bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font linux-headers linux-zen linux-zen-headers
