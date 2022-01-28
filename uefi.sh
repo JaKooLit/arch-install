026b1013daf4bdd1e15fa3ef9a547c3fe0d90399
Ja.KooLit <85185940+JaKooLit@users.noreply.github.com>
2022-01-28 17:41:15 +0900

    Update uefi.sh

--- uefi.sh
+++ uefi.sh
@@ -11,7 +11,10 @@ echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 Asus-Arch.localdomain Asus-Arch" >> /etc/hosts
 echo root:password | chpasswd
 
-pacman -S grub base-devel efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils alsa-utils easyeffects pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call  bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font linux-headers linux-zen linux-zen-headers pipewire-x11-bell realtime-privileges
+pacman -S grub base-devel efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call  bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font linux-headers linux-zen linux-zen-headers pipewire-x11-bell realtime-privileges
+
+# easyeffects from repo install alot of plugin so either that or easyeffects-git (From AUR)
+sudo pacman -S easyeffects
 
 
 # extra utils
