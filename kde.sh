89f25bf516265e7810a86acfa6846539d7356099
Ja.KooLit <85185940+JaKooLit@users.noreply.github.com>
2022-02-21 10:53:43 +0900

    Update kde.sh

--- kde.sh
+++ kde.sh
@@ -11,17 +11,19 @@ sudo firewall-cmd --add-port=1025-65535/udp --permanent
 sudo firewall-cmd --reload
 
 #edit the packages as required
-sudo pacman -S xorg sddm plasma-desktop konsole packagekit-qt5 kwallet-pam noto-fonts ark ttf-hack adobe-source-han-sans-otc-fonts \ 
-adobe-source-han-serif-otc-fonts noto-fonts-cjk powerdevil plasma-nm bluedevil firefox spectacle gwenview vlc kate dolphin \ 
-dolphin-plugins powerline-fonts discover sddm-kcm print-manager khotkeys kscreen kgamma5 kinfocenter ksystemlog filelight
+sudo pacman -S xorg sddm plasma-desktop konsole packagekit-qt5 kwallet-pam noto-fonts ark ttf-hack adobe-source-han-sans-otc-fonts
+
+sudo pacman -S adobe-source-han-serif-otc-fonts noto-fonts-cjk powerdevil plasma-nm bluedevil firefox spectacle gwenview vlc kate dolphin
+
+sudo pacman -S dolphin-plugins powerline-fonts discover sddm-kcm print-manager khotkeys kscreen kgamma5 kinfocenter ksystemlog filelight
 
 #wayland
 # sudo pacman -S xdg-desktop-portal-kde plasma-wayland-session kvantum
 
 #additional if required
-#
+
 sudo systemctl enable sddm
 
-/bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
+echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
 #sleep 5
-reboot
+#reboot
