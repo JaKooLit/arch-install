bdeb592e1db4eea275bd1bd7bfc9acc97f487ce0
Ja.KooLit <85185940+JaKooLit@users.noreply.github.com>
2023-02-08 16:08:09 +0900

    Update uefi.sh

--- uefi.sh
+++ uefi.sh
@@ -20,7 +20,8 @@ echo root:1 | chpasswd
 pacman -S grub base-devel efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools avahi xdg-user-dirs xdg-utils gvfs
 pacman -S gvfs-smb nfs-utils inetutils dnsutils alsa-utils bash-completion rsync reflector acpi openssh
 pacman -S acpi_call bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft firewalld sof-firmware nss-mdns acpid ntfs-3g terminus-font 
-pacman -S linux-headers  
+pacman -S linux-headers linux-zen linux-zen-headers grub-btrfs blueman zram-generator
+pacman -S blueman blueman-applet blueman-manager
 
 #pipewire
 pacman -S pipewire pipewire-alsa pipewire-pulse pipewire-jack pipewire-x11-bell realtime-privileges wireplumber 
@@ -47,7 +48,8 @@ pacman -S pipewire pipewire-alsa pipewire-pulse pipewire-jack pipewire-x11-bell 
 # pacman -S xf86-video-amdgpu 
 # pacman -S nvidia nvidia-utils nvidia-settings
 
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=UEFI-Arch
+#note! remember to identify your proper esp partition. This setup is separate efi and boot partition
+grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=UEFI-Arch
 grub-mkconfig -o /boot/grub/grub.cfg
 
 systemctl enable NetworkManager
