83e2ea67514b21136b80739a03de0fc999395017
Ja.KooLit <85185940+JaKooLit@users.noreply.github.com>
2023-02-20 18:25:56 +0900

    Update gnome-wayland.sh

--- gnome-wayland.sh
+++ gnome-wayland.sh
@@ -15,11 +15,14 @@ sudo firewall-cmd --reload
 #systemctl start auto-cpufreq
 
 #minimal-gnome
-sudo pacman -S gnome-shell nautilus gnome-terminal guake gnome-tweak-tool gnome-control-center xdg-user-dirs gdm eog gnome-menus
-sudo pacman -S firefox gnome-browser-connector gnome-screenshot gnome-shell-extensions gnome-tweaks arc-gtk-theme arc-icon-theme vlc dina-font tamsyn-font ttf-bitstream-vera ttf-croscore ttf-dejavu ttf-droid gnu-free-fonts ttf-ibm-plex ttf-liberation ttf-linux-libertine noto-fonts ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family ttf-anonymous-pro ttf-cascadia-code ttf-fantasque-sans-mono ttf-fira-mono ttf-hack ttf-fira-code ttf-inconsolata ttf-jetbrains-mono ttf-monofur adobe-source-code-pro-fonts cantarell-fonts inter-font ttf-opensans gentium-plus-font ttf-junicode adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk noto-fonts-emoji
+sudo pacman -S gnome-shell nautilus gnome-terminal guake gnome-tweak-tool gnome-control-center xdg-user-dirs gdm eog gnome-menus firemanager-actions
+sudo pacman -S firefox gnome-browser-connector gnome-screenshot gnome-shell-extensions gnome-tweaks arc-gtk-theme arc-icon-theme vlc 
+
+# fonts edit as required
+sudo pacman -S ttf-droid gnu-free-fonts tf-liberation ttf-linux-libertine noto-fonts ttf-roboto ttf-ubuntu-font-family ttf-anonymous-pro ttf-cascadia-code ttf-fantasque-sans-mono ttf-fira-mono ttf-hack ttf-fira-code ttf-inconsolata ttf-jetbrains-mono ttf-monofur adobe-source-code-pro-fonts cantarell-fonts inter-font ttf-opensans gentium-plus-font ttf-junicode adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk noto-fonts-emoji
 
 #sudo pacman -S gnome-extra gnome 
-sudo pacman -S xdg-desktop-portal-gtk
+sudo pacman -S xdg-desktop-portal-gtk xdg-desktop-portal
 
 # sudo pacman -S --noconfirm xorg
 
